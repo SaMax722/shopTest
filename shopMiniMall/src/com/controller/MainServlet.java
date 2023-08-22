3743efe9ca690258cbbdefe764edb91776d23baa
KimSangWon <kimsang1220@gmail.com>
2023-08-22 09:55:22 +0900

    forward main.jsp

--- shopMiniMall/src/com/controller/MainServlet.java
+++ shopMiniMall/src/com/controller/MainServlet.java
@@ -12,14 +12,10 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	// 포워드
+		request.getRequestDispatcher("main.jsp").forward(request, response);
 	}
 
 }
